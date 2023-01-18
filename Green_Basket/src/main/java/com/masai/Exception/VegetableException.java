030a047d4a49494a28f704a83b42af62345faa7f
Nitesh Kumar Pal <nitesh28rocking@gmail.com>
2023-01-18 23:12:51 +0530

    vegetable model done

---
+++ Green_Basket/src/main/java/com/masai/Exception/VegetableException.java
@@ -0,0 +1,9 @@
+package com.masai.Exception;
+
+public class VegetableException extends RuntimeException{
+
+	
+	public VegetableException (String s) {
+		super(s);
+	}
+}
---
+++ Green_Basket/src/main/java/com/masai/Model/VegetableDto.java
@@ -0,0 +1,8 @@
+package com.masai.Model;
+
+public class VegetableDto {
+
+	
+	
+		
+}
