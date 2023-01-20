2292a97744d511aa71cffcfc4fc87ce61db768aa
Mayank Gupta <107461052+mynkgupta22@users.noreply.github.com>
2023-01-20 19:21:11 +0530

    added

--- Green_Basket/src/main/java/com/masai/Model/Cart.java
+++ Green_Basket/src/main/java/com/masai/Model/Cart.java
@@ -3,21 +3,14 @@ package com.masai.Model;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Cart {
@@ -26,11 +19,15 @@ public class Cart {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer cartId;
 	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "customerId")
+//	@OneToOne(cascade = CascadeType.ALL)
+//	@JoinColumn(name = "customerId")
 	private Integer customerId;
-	
-	@OneToMany(cascade = CascadeType.ALL)
-	private List<VegetableDTO> vegetable = new ArrayList<>();
+
+	@OneToOne(cascade = CascadeType.ALL)
+	private Order order;
+
+	@Embedded
+	private List<VegetableDTO> vegetable=new ArrayList<>();
+
 
 }
--- Green_Basket/src/main/java/com/masai/Model/Vegetable.java
+++ Green_Basket/src/main/java/com/masai/Model/Vegetable.java
@@ -1,6 +1,8 @@
 
 package com.masai.Model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.validation.constraints.Size;
 import javax.persistence.GeneratedValue;
@@ -11,57 +13,39 @@ import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Vegetable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Integer VegId;
-	@NotNull
-	@Size(min=3,max =10	,message = "length should be 3 to 10")
-	private String Name;
-	@NotNull
-	@Size(min=3,max =10	,message = "length should be 3 to 10")
-	private String Type;
-	@NotNull
-	@Size(min=4,max =10	,message = "length should be 4 to 10")
-	private String seasonality;
-	@NotNull
-	@Size(min=10,max =50	,message = "nutrition desc should be 10 to 50")
-	private String nutrition_Details;
-	@NotNull
-	@Size(min=3,max =10	,message = "Storage should be 3 to 10")
-	private String storage;
-	
+	private Integer vegId;
+	//@NotNull
+	//@Size(min=3,max =10	,message = "length should be 3 to 10")
+
+	private String name;
+	//@NotNull
+	//@Size(min=3,max =10	,message = "length should be 3 to 10")
+
+	private String type;
+	//@NotNull
+	//@Size(min=4,max =10	,message = "length should be 4 to 10")
+
+
+	//@NotNull
+	//@Size(min=3,max =50	,message = "descrption length should be 3 to 50")
+	private String descrption;
+
+	//@NotNull
+//	@Size(min=1,max=500, message = "unit should be greater than 0 ")
+	private int quantity;
+
+	//@NotNull
+//	@DecimalMin("1")
+//	@DecimalMax("10000")
+	private Double price;
 
 
 
-
-	public Vegetable(Integer vegId, @NotNull @Size(min = 3, max = 10, message = "length should be 3 to 10") String name,
-			@NotNull @Size(min = 3, max = 10, message = "length should be 3 to 10") String type,
-			@NotNull @Size(min = 4, max = 10, message = "length should be 4 to 10") String seasonality,
-			@NotNull @Size(min = 10, max = 50, message = "nutrition desc should be 10 to 50") String nutrition_Details,
-			@NotNull @Size(min = 3, max = 10, message = "Storage should be 3 to 10") String storage,
-			@NotNull @Size(min = 1, max = 10, message = "shel_lyf should be 1 day to 10 days") String shelf_life,
-			@NotNull @Size(min = 3, max = 50, message = "descrption length should be 3 to 50") String descrption,
-			@NotNull int quantity, @NotNull Double price) {
-		super();
-
-		this.VegId = vid;
-		this.Name = veg_name;
-		this.Type = veg_type;
-
-		this.seasonality = seasonality;
-		this.nutrition_Details = nutrition_Details;
-		this.storage = storage;
-		this.shelf_life = shelf_life;
-		this.descrption = descrption;
-    this.Quantity = unit;
-		this.Price = price;
-
-	}
-
-
-
-	
-	
 }
