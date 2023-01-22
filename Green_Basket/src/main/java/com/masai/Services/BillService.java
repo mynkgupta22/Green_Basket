55a436738a52fd5c0b7b0fd59aa9a39aa37d2db7
Santosh2704 <santoshvishwa1572@gmail.com>
2023-01-22 20:07:51 +0530

    Done!

---
+++ Green_Basket/src/main/java/com/masai/Services/BillService.java
@@ -0,0 +1,19 @@
+package com.masai.Services;
+
+import java.util.List;
+
+import com.masai.Exception.BillException;
+import com.masai.Model.BillServiceModel;
+
+public interface BillService {
+
+	public BillServiceModel BillsAdded(BillServiceModel addBills) throws BillException;
+
+	public List<BillServiceModel> getAllBill() throws BillException;
+
+
+
+	
+	
+	
+}
---
+++ Green_Basket/src/main/java/com/masai/Services/BillServiceImpl.java
@@ -0,0 +1,54 @@
+package com.masai.Services;
+
+import java.util.List;
+
+import com.masai.Exception.BillException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.masai.Repository.BillserviceDao;
+import com.masai.Model.BillServiceModel;
+
+@Service
+public class BillServiceImpl implements BillService {
+
+	@Autowired
+	private BillserviceDao repo;
+
+	@Override
+	public BillServiceModel BillsAdded(BillServiceModel addBills) throws BillException {
+
+		if(addBills == null){
+			throw new BillException("Bill Can't be Null");
+		}
+		BillServiceModel f1=repo.save(addBills);
+		return f1;
+	}
+
+	@Override
+	public List<BillServiceModel> getAllBill() throws BillException{
+		
+		return repo.findAll();
+	}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
