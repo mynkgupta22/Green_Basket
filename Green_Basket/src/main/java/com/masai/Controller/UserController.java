02d8ddf924f4606c7ffa01a84bff0925b885a5a6
kieransahoo <kkieran50@gmail.com>
2023-01-22 15:13:19 +0530

    login frontend added

--- Green_Basket/src/main/java/com/masai/Controller/UserController.java
+++ Green_Basket/src/main/java/com/masai/Controller/UserController.java
@@ -3,10 +3,7 @@ package com.masai.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.masai.Exception.UserException;
 import com.masai.Model.User;
@@ -17,6 +14,7 @@ import com.masai.Services.UserLoginService;
 
 
 @RestController
+@CrossOrigin
 public class UserController {
 	
 	@Autowired
